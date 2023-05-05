268c2f23a38f5041e71cb4788e0aba53697b0e0e
cristian200244 <moraesangel968@gmail.com>
2023-05-04 19:30:28 -0500

    ajuste plantilla

---
+++ views/main/partials/footer.php
@@ -0,0 +1,20 @@
+ <!-- Footer -->
+ <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2020</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
