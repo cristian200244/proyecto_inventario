ae0aba8153c719d58b2141147a8bb36bbe05bc77
juliank2000 <barriosjulian246@gmail.com>
2023-05-08 22:50:35 -0500

    Update index.php

--- views/dispositivos/index.php
+++ views/dispositivos/index.php
@@ -3,14 +3,35 @@ include_once(__DIR__ . "../../../config/config.php");
 include_once(BASE_DIR . '../../views/main/partials/header.php');
 ?>
 
-<!-- Begin Page Content -->
-<div class="container-fluid">
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">hola estas en el modulo Dispositivos</h1>
 
-</div>
-<!-- /.container-fluid -->
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <?php
 include_once(BASE_DIR . '../../views/main/partials/footer.php');
