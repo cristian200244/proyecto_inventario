c74b317682b8481d473d7a4c6cbbb5bec6c63c36
juliank2000 <barriosjulian246@gmail.com>
2023-05-10 21:49:43 -0500

    cambios

--- views/main/partials/aside.php
+++ views/main/partials/aside.php
@@ -1,16 +1,16 @@
- <!-- Sidebar -->
- <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion " id="accordionSidebar">
+  <!-- Sidebar -->
+  <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion " id="accordionSidebar">
 
-     <!-- Sidebar - Brand -->
-     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= BASE_URL ?>./views/main/index.php">
-         <div class="sidebar-brand-icon rotate-n-15">
-             <i class="fas fa-laugh-wink"></i>
-         </div>
-         <div class="sidebar-brand-text mx-3">Electrónica L&L </div>
-     </a>
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= BASE_URL ?>./views/main/index.php">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Electrónica L&L </div>
+</a>
 
-     <!-- Divider -->
-     <hr class="sidebar-divider my-0">
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
 
      <!-- Nav Item - Dashboard -->
      <li class="nav-item">
@@ -54,12 +54,6 @@
          <div class="dropdownlink text-light"><i class="ml-3" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-motherboard" viewBox="0 0 16 16">
                      <path d="M11.5 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm2 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm-10 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6ZM5 3a1 1 0 0 0-1 1h-.5a.5.5 0 0 0 0 1H4v1h-.5a.5.5 0 0 0 0 1H4a1 1 0 0 0 1 1v.5a.5.5 0 0 0 1 0V8h1v.5a.5.5 0 0 0 1 0V8a1 1 0 0 0 1-1h.5a.5.5 0 0 0 0-1H9V5h.5a.5.5 0 0 0 0-1H9a1 1 0 0 0-1-1v-.5a.5.5 0 0 0-1 0V3H6v-.5a.5.5 0 0 0-1 0V3Zm0 1h3v3H5V4Zm6.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2Z" />
                      <path d="M1 2a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-2H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 9H1V8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6H1V5H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 2H1Zm1 11a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v11Z" />
-<<<<<<<<< Temporary merge branch 1
-                 </svg></i>
-                 <span>Dispositivos</span>
-         </a>
-       
-=========
                  </svg></i>     Dispositivos
              <i class="fa fa-chevron-down ml-4" aria-hidden="true"></i>
          </div>
@@ -79,15 +73,14 @@
                         <ul>
 
                             <li><a href="<?= BASE_URL ?>./views/dispositivos/index.php" class="text-light text-decoration-none" >Ver Reportes</a></li>
-                            <li><a href=" #" class="text-light text-decoration-none" >Crear Factura</a></li>
-                            <li><a href=" #" class="text-light text-decoration-none" >Ver Factura</a></li>
+                            <li><a href=" ··" class="text-light text-decoration-none" >Crear Factura</a></li>
+                            <li><a href="··" class="text-light text-decoration-none" >Ver Factura</a></li>
                         </ul>
                     </ul>
      </li>
 
-      
  
 
 
 
- </ul>
+</ul>
