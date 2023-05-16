f7ac104ed51964790a71f8c1cf55cd3307f5347f
Olver Yucuna <yucunaolvertanimuca@gmail.com>
2023-05-16 18:43:36 -0500

    admin link

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,13 +1,14 @@
 <?php
 include_once(__DIR__ . "../../../config/config.example.php");
 include_once(BASE_DIR . '../../views/main/partials/header.php');
+include_once(BASE_DIR . '../../views/main/partials/header.php');
 ?>
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Clientes <button type="button" class="btn btn-outline-primary float-right mr-5"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
+    <h1 class="h3 mb-4 text-gray-800">Usuario <button type="button" class="btn btn-outline-primary float-right mr-5"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
                 <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
                 <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
             </svg><a href="create.php"></a></button></h1>
@@ -26,12 +27,14 @@ include_once(BASE_DIR . '../../views/main/partials/header.php');
                 <th scope="col" colspan="3">Opciones</th>
             </tr>
         </thead>
-        <tbody>
-            <button type="submit" class="btn btn-outline-info ml-0">Guardar Cliente</button>
-        </div>
-    </form>
+        
+        
+        </table>
+
+</form>
 
 </div>
+
 <!-- /.container-fluid -->
 
 <?php
