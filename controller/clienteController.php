aedc01c359bc730b4b2b553524936bffc9bbb9ed
cristian200244 <moraesangel968@gmail.com>
2023-06-05 22:59:35 -0500

    ajueste en mvc de clientes lineas comentadas

--- controller/clienteController.php
+++ controller/clienteController.php
@@ -17,7 +17,7 @@ class ClienteController
         if (isset($_REQUEST['c'])) {
             switch ($_REQUEST['c']) {
                 case '1': //Almacenar en la base de datos
-                    self::store();
+                    // self::store();
 
                     break;
                 case '2': //ver usuario
@@ -41,34 +41,41 @@ class ClienteController
         return  $this->cliente->getAll();
     }
 
-    public function store()
-    {
-
-
-
-        $datos = [
-
-            'id_tipo_documento' => $_REQUEST['id_tipo_documento'],
-            'numero_documento'  => $_REQUEST['numero_documento'],
-            'primer_nombre'     => $_REQUEST['primer_nombre'],
-            'segundo_nombre'    => $_REQUEST['segundo_nombre'],
-            'primer_apellido'   => $_REQUEST['primer_apellido'],
-            'segundo_apellido'  => $_REQUEST['segundo_apellido'],
-            'id_sexo'           => $_REQUEST['id_sexo'],
-            'id_ciudad'         => $_REQUEST['id_ciudad'],
-            'telefono'          => $_REQUEST['telefono'],
-            'correo'            => $_REQUEST['correo'],
-            'direccion'         => $_REQUEST['direccion'],
-        ];
-
-        $result = $this->cliente->store($datos);
-        if ($result) {
-            header("Location: ../views/clientes/index.php");
-            exit();
-        } else {
-            echo $error = "Ocurrió un error";
-        }
-    }
+    // public function store()
+    // {   
+    //     if ($_SERVER['SERVER_METHOD'] == 'POST') {
+    //         $primer_nombre= $_POST['primer_nombre'];
+        
+    //         $cliente = new ClienteController();
+    //         if ($cliente->existeNombre($primer_nombre)) {
+    //             echo "este usuario  esta repetido";
+    //         }else{
+    //         }
+    //         $datos = [
+    
+    //             'id_tipo_documento' => $_REQUEST['id_tipo_documento'],
+    //             'numero_documento'  => $_REQUEST['numero_documento'],
+    //             'primer_nombre'     => $_REQUEST['primer_nombre'],
+    //             'segundo_nombre'    => $_REQUEST['segundo_nombre'],
+    //             'primer_apellido'   => $_REQUEST['primer_apellido'],
+    //             'segundo_apellido'  => $_REQUEST['segundo_apellido'],
+    //             'id_sexo'           => $_REQUEST['id_sexo'],
+    //             'id_ciudad'         => $_REQUEST['id_ciudad'],
+    //             'telefono'          => $_REQUEST['telefono'],
+    //             'correo'            => $_REQUEST['correo'],
+    //             'direccion'         => $_REQUEST['direccion'],
+    //         ];
+    
+    //         $result = $this->cliente->store($datos);
+    //         if ($result) {
+    //             header("Location: ../views/clientes/index.php");
+    //             exit();
+    //         } else {
+    //             echo $error = "Ocurrió un error";
+    //         }
+    //         }
+        
+    // }
 
     public function show()
     {
@@ -85,7 +92,7 @@ class ClienteController
     public function update()
     {
         $datos = [
-            'id_persona'                => $_REQUEST['id_persona'],
+            'id_persona'        => $_REQUEST['id_persona'],
             'id_tipo_documento' => $_REQUEST['id_tipo_documento'],
             'numero_documento'  => $_REQUEST['numero_documento'],
             'primer_nombre'     => $_REQUEST['primer_nombre'],
--- views/main/partials/menu.php
+++ views/main/partials/menu.php
@@ -3,7 +3,7 @@
 <a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/tipo_dispositivos">Dispositivos</a> 
 <a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/estado_producto/index.php">Estado Del Dispositivo</a> 
 <a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/tipo_documento/index.php">Tipos De Documento</a> 
-<a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/tipo_servicio/index.php">Tipo De Servicios</a> 
-<a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/ciudad/index.php">Departamentos</a>   
+<a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/tipo_servicio/index.php">Tipos De Servicios</a> 
+<a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/ciudad/index.php">Ciudades</a>   
 <a type="button" class="btn btn-info text-decoration-none" href="<?= BASE_URL ?>./views/sexo/index.php">Sexo</a>  
  
